a2930cd63d1d08600b395a66f9d8b57bd657937d
threesond <chuiniube@qq.com>
2018-11-05 10:24:59 +0800

    Update train.py

--- train.py
+++ train.py
@@ -17,15 +17,20 @@ def get_data():
 tf.reset_default_graph()
 sess = tf.Session()
 
+#读取训练数据
 train_data_path = cfg.train_dir+'/train_vocoder.npy'
 train_data = np.load(train_data_path).item()['data']
+#只使用一个训练数据来测试模型能不能过拟合
 train_data = train_data[:1]
 
+#输入数据,输入数据位置和标签数据的placeholder
 input_phone = tf.placeholder(tf.int32,[None],name='input')
 input_features = tf.placeholder(tf.float32,[cfg.sp_dim+2,None])
 input_pos = tf.placeholder(tf.int32,[None],name='pos')
+#得到模型输出
 logit = net(input_phone,input_pos)
 
+#定义loss和train
 loss_op = tf.reduce_mean(tf.abs(logit - input_features))
 train_op = tf.train.AdamOptimizer(0.0001).minimize(loss_op)
 
